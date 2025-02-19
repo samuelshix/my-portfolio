fba6b5e9ef33d3a6e89e96dde64f6b4d335ca68a
samshi <samshi@iu.edu>
2025-02-19 15:48:33 -0500

    fix lint

--- components/Socials/index.js
+++ components/Socials/index.js
@@ -7,9 +7,9 @@ const Socials = ({ className }) => {
     return (
         <div className={`${className} text-black mob:flex-nowrap border-white/50`}>
             {yourData.socials.map((social, index) => (
-                <div className="flex flex-row text-white">
+                <div className="flex flex-row text-white" key={index}>
                     <p className="mr-2">{social.title}:</p>
-                    <div className="text-white font-light hover:text-yellow-100 transition-all duration-300 cursor-pointer mb-2" key={index} onClick={() => window.open(social.link)}>
+                    <div className="text-white font-light hover:text-yellow-100 transition-all duration-300 cursor-pointer mb-2" onClick={() => window.open(social.link)}>
                         {social.link}
                     </div>
                 </div>
